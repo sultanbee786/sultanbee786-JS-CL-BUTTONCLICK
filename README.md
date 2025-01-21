cee491914e49093401bcdc02fdcbc11677f97293
sultanbee786 <sulthanbeeshaik31@gmail.com>
2025-01-21 20:42:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# JS Button Click Lab
+
+
+## Description
+
+In this lab you'll learn how some JavaScripts basics. You'll select HTML elements in JavaScript, then add events to add functionality to the html page.
+
+
+## Where to start
+
+Begin by opening and viewing the "ButtonClicker.html" file; the file itself contains this lab's instructions.
+
+Files to edit:
+- ButtonClicker.html
+
+
+
+
+
---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
+
+        // Get file
+        File file = new File("src/main/ButtonClicker.html");
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+        // Open the HTML file
+        webDriver.get(path);
+    }
+
+    @Test
+    public void testButton1() {
+        // check the initial state of the text:
+        WebElement displayElement = webDriver.findElement(By.id("text1"));
+        assertEquals("click the button ...", displayElement.getText());
+
+        // Find button 1 and click it
+        WebElement button1 = webDriver.findElement(By.id("button1"));
+        button1.click();
+
+        assertEquals("button 1 clicked", displayElement.getText());
+    }
+
+    @Test
+    public void testButton2() {
+        // check the initial state of the text:
+        WebElement displayElement = webDriver.findElement(By.id("text2"));
+        assertEquals("now click this button ...", displayElement.getText());
+
+        // Find button 2 and click it
+        WebElement button2 = webDriver.findElement(By.id("button2"));
+        button2.click();
+
+        assertEquals("button 2 clicked", displayElement.getText());
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        webDriver.quit();
+    }
+}
